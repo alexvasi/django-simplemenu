8d663ca5576fff903825e0d4d8ee4f6725f2895c
Alex Vasi <eee@someuser.com>
2010-04-07 01:07:59 +0400

    Saves results of get_registered_pages() in MenuItemForm.

--- simplemenu/forms.py
+++ simplemenu/forms.py
@@ -20,18 +20,19 @@ class MenuItemForm(forms.ModelForm):
         super(MenuItemForm, self).__init__(data, files, auto_id, prefix,
                                            initial, error_class, label_suffix,
                                            empty_permitted, instance)
+        self._registered_pages_cache = get_registered_pages()
         self.fields['page'].choices = self.page_choices()
 
     def page_choices(self):
         choices = []
-        for p in get_registered_pages():
+        for p in self._registered_pages_cache:
             choices.append((p.strkey(), p.name()))
         choices.sort(key=lambda x: x[1])
         return choices
 
     def selected_page(self):
         key = self.cleaned_data['page']
-        for p in get_registered_pages():
+        for p in self._registered_pages_cache:
             if p.strkey() == key:
                 return p
         return None
