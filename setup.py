9119bc8dad3afa23df061ba71c197d761efc83aa
Alex Vasi <eee@someuser.com>
2010-04-17 12:40:10 +0400

    Added package version and more info to setup.py.

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 from distutils.core import setup
+import simplemenu
 
 setup(name='django-simplemenu',
-      version='0.0.1',
+      version=simplemenu.__version__,
+      url='http://github.com/alexvasi/django-simplemenu',
+      license='BSD',
+      description='Menu app for Django with ordering and ability to link menu item with model instance, view or URL.',
+      author='Alex Vasi',
+      author_email='eee@someuser.com',
       packages=['simplemenu'])
--- simplemenu/__init__.py
+++ simplemenu/__init__.py
@@ -1,2 +1,4 @@
 from simplemenu.pages import register
 
+__version__ = "0.1.0"
+
